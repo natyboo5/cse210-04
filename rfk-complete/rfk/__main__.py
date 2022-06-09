b1f48052cd639fad30f30951bdc1507c3f268d50
Edgar Valderrama <valderrama311@gmail.com>
2022-06-09 13:57:54 -0500

    main file changes

--- rfk-complete/rfk/__main__.py
+++ rfk-complete/rfk/__main__.py
@@ -22,16 +22,15 @@ FONT_SIZE = 15
 COLS = 60
 ROWS = 40
 CAPTION = "Robot Finds Kitten"
-DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
 WHITE = Color(255, 255, 255)
 DEFAULT_ARTIFACTS = 40
 
 
 def main():
-    
+
     # create the cast
     cast = Cast()
-    
+
     # create the banner
     banner = Actor()
     banner.set_text("")
@@ -39,7 +38,7 @@ def main():
     banner.set_color(WHITE)
     banner.set_position(Point(CELL_SIZE, 0))
     cast.add_actor("banners", banner)
-    
+
     # create the robot
     x = int(MAX_X / 2)
     y = int(MAX_Y / 2)
@@ -51,15 +50,11 @@ def main():
     robot.set_color(WHITE)
     robot.set_position(position)
     cast.add_actor("robots", robot)
-    
-    # create the artifacts
-    with open(DATA_PATH) as file:
-        data = file.read()
-        messages = data.splitlines()
 
+     # create the artifacts
     for n in range(DEFAULT_ARTIFACTS):
-        text = chr(random.randint(33, 126))
-        message = messages[n]
+        elements = ['O', '*']
+        text = elements[random.randint(0, 1)]
 
         x = random.randint(1, COLS - 1)
         y = random.randint(1, ROWS - 1)
@@ -70,15 +65,15 @@ def main():
         g = random.randint(0, 255)
         b = random.randint(0, 255)
         color = Color(r, g, b)
-        
+
         artifact = Artifact()
+        artifact.set_velocity(Point(0,3))
         artifact.set_text(text)
         artifact.set_font_size(FONT_SIZE)
         artifact.set_color(color)
         artifact.set_position(position)
-        # artifact.set_message(message)
         cast.add_actor("artifacts", artifact)
-    
+
     # start the game
     keyboard_service = KeyboardService(CELL_SIZE)
     video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
